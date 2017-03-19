a6bfcbb9c250b683e9ef7ef7612ab1dfa342c32b
Akshay Nair <hello@akshaynair.com>
2017-03-18 23:23:55 -0400

    Flask update

--- genetic/webserver.py
+++ genetic/webserver.py
@@ -1,10 +1,15 @@
-from flask import Flask, request, render_template
-app = Flask(__name__)
+from flask import Flask, request, render_template, send_from_directory
+app = Flask(__name__, static_folder='templates')
 
 @app.route('/')
 def Tetris():
     return render_template('index.html')
 
+@app.route('/<path:path>')
+def send_static(path):
+    return send_from_directory(app.static_folder, path)
+
 if __name__ == '__main__':
     app.debug = True
-    app.run(host = '0.0.0.0', port = 5000)
+    app.run(host = '0.0.0.0', port = 5000, threaded = True)
+
