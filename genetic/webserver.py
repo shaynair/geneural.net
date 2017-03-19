8a43260c9cadf225f7b209a52e34556690c1d86e
Akshay Nair <hello@akshaynair.com>
2017-03-19 04:58:20 -0400

    Remove RPC redundancy

--- genetic/webserver.py
+++ genetic/webserver.py
@@ -1,7 +1,5 @@
 from flask import Flask, request, render_template, send_from_directory
-app = Flask(__name__, static_folder='templates')
-client = zerorpc.Client(timeout=30000, heartbeat=None)
-ENDPOINT=4000
+app = Flask(__name__, static_folder='templates'
 
 @app.route('/')
 def Tetris():
@@ -12,7 +10,6 @@ def send_static(path):
     return send_from_directory(app.static_folder, path)
 
 if __name__ == '__main__':
-    client.connect('tcp://127.0.0.1:' + str(ENDPOINT))
     app.debug = True
     app.run(host = '0.0.0.0', port = 5000, threaded = True)
 
