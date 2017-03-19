b6e3196d7e044139d3bb0048526c2cc5a18e32c3
Akshay Nair <hello@akshaynair.com>
2017-03-19 08:50:37 -0400

    Add another chart.

--- genetic/templates/js/chart.js
+++ genetic/templates/js/chart.js
@@ -58,7 +58,7 @@ $(document).ready(function() {
                         point: {
                             events: {
                                 click: function(e) {
-                                    Tetris.init(replays[this.x - 1], false);
+                                    Tetris.init(replays[this.x - 1].code, false);
                                 }
                             }
                         },
@@ -69,8 +69,88 @@ $(document).ready(function() {
                 },
 
                 series: [{
-                    name: 'Score'
+                    name: 'Turns Lasted'
                 }]
+            });
+
+
+            $.get('data/weights.csv', function(weights) {
+                Highcharts.chart('container-weights', {
+
+                    data: {
+                        csv: weights
+                    },
+
+                    title: {
+                        text: 'Genetic Algorithm Weights'
+                    },
+
+                    subtitle: {
+                        text: 'Generations vs. Weights'
+                    },
+
+                    xAxis: {
+                        tickInterval: 1, // one gen.
+                        tickWidth: 0,
+                        gridLineWidth: 1,
+                        labels: {
+                            align: 'left',
+                            x: 3,
+                            y: -3
+                        }
+                    },
+
+                    yAxis: [{ // left y axis
+                        title: {
+                            text: null
+                        },
+                        labels: {
+                            align: 'left',
+                            x: 3,
+                            y: 16,
+                            format: '{value:.,0f}'
+                        }
+                    }],
+
+                    legend: {
+                        align: 'left',
+                        verticalAlign: 'top',
+                        y: 20,
+                        floating: true,
+                        borderWidth: 0
+                    },
+
+                    tooltip: {
+                        shared: true,
+                        crosshairs: true
+                    },
+
+                    plotOptions: {
+                        series: {
+                            cursor: 'pointer',
+                            point: {
+                                events: {
+                                    click: function(e) {
+                                        Tetris.init(replays[this.x - 1].code, false);
+                                    }
+                                }
+                            },
+                            marker: {
+                                lineWidth: 1
+                            }
+                        }
+                    },
+
+                    series: [{
+                        name: 'Holes (weight)'
+                    }, {
+                        name: 'Average Height (weight)'
+                    }, {
+                        name: 'Blocks above Holes (weight)'
+                    }, {
+                        name: 'Gaps (weight)'
+                    }]
+                });
             });
         });
     }, 'text');
